3b591365dd83b27d0de0b6fcb5c3674a6780ee41
eldrgeek <mw@mike-wolf.com>
2025-07-20 19:07:08 -0600

    Build change

--- front-row-vite/src/buildInfo.ts
+++ front-row-vite/src/buildInfo.ts
@@ -1,1 +1,1 @@
-export const BUILD_INFO = { version: '1.2.0', buildTime: '2025-07-20T18:48:32Z', commit: '4fb2fa2', branch: 'master' };
+export const BUILD_INFO = { version: '1.2.0', buildTime: '2025-07-21T01:05:51Z', commit: '957e911', branch: 'master' };
